cfb60e7e220bdf90b07f9146a019f6db6f672f61
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-03-25 13:04:58 -0500

    working version0.1

---
+++ main.js
@@ -0,0 +1,65 @@
+// custom js file
+
+/*
+    1. create a new instance of decibel.
+    2. create a var to store the returned audioBuffer objects
+    3. call decodeAudioFiles on an array of links 
+        i. ["../audioFiles/example.mp3"] return {src-1 : "../audioFiles/example.mp3" }
+        ii. ["example1", "../audioFiles/example.mp3"] returs { example1 : "../audioFiles/example.mp3"}
+    4. call the playback method on a file to play the file once without further processing
+        
+*/
+
+// create a new instance of decibel
+var audio = new decibel();
+
+// var to store proccessed audio
+var audioFiles;
+
+// demo audio sources
+var testSource = [                                  // **demo**
+    '../audioFiles/simonSound1.mp3',                // string
+    ['src2','../audioFiles/simonSound2.mp3'],      // array
+    '../audioFiles/simonSound3.mp3',                // string
+    ['src4','../audioFiles/simonSound4.mp3']       // array    
+];
+
+// decode audio files
+audio.decodeAudioFiles(testSource).then(function(decoded){
+    // success
+    audioFiles = decoded;
+    console.log("audioFiles loaded");
+}).catch(function(err){
+    // error
+    console.error(err);
+});
+
+
+//===============================================================
+// playback audio files
+
+function playSound(button) {
+    switch (button) {
+        case 1 : audio.playback(audioFiles.src1); break;
+        case 2 : audio.playback(audioFiles.src2); break;
+        case 3 : audio.playback(audioFiles["src3"]); break;
+        case 4 : audio.playback(audioFiles["src4"]); break;
+        default : console.error("bad input");
+    }
+}
+
+var buttonOne = document.getElementById("button1").addEventListener('click', function(){
+    playSound(1);
+});
+var buttonTwo = document.getElementById("button2").addEventListener('click', function(){
+    playSound(2);
+});
+var buttonThree = document.getElementById("button3").addEventListener('click', function(){
+    playSound(3);
+});
+var buttonFour = document.getElementById("button4").addEventListener('click', function(){
+    playSound(4);
+});
+
+
+
---
+++ versions/version_0.1.js
@@ -0,0 +1,96 @@
+var ctx; // global webAudio api var
+
+window.addEventListener('load', init, false);
+
+// init the audio context
+function init(){
+    try {
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        ctx = new AudioContext();
+        console.log("AudioContext created!");
+    }
+    catch(e) {
+        alert("Web Audio not supported in your browser, please try a newer release of Safari, Chrome, or Firefox");
+        }
+}
+
+// decibel constrctor
+function decibel(){
+    
+    // decode audio files
+    this.decodeAudioFiles = function(audioSources){
+        return new Promise(function(resolve, reject){
+       
+            // store pending promises
+            var reqPromises = [];
+
+            // preprocess the current file
+            function prepFile(file, source){
+                if(typeof file === "string") return ["src" + (source.indexOf(file) + 1), file]; 
+                else if(typeof file[0] === "string") return [file[0], file[1]];
+                else throw new Error(file + ", at index: "+ source.indexOf(file) +" is not of correct type");
+            } // prep
+            
+            // httpReq
+            function httpReq(file, source){
+                return new Promise(function(resolve, reject){
+                    var preppedFile = prepFile(file, source);
+                    if(preppedFile){
+                        var req = new XMLHttpRequest();
+                        req.open('GET', preppedFile[1], true);
+                        req.responseType = 'arraybuffer';
+                        req.onload = function(){
+                            if(req.readyState === 4){
+                                if(req.status === 200){
+                                    ctx.decodeAudioData(req.response, function(buffer){
+                                       resolve([preppedFile[0], buffer]); 
+                                    }, function(err){
+                                        throw new Error("failed to decode: " + preppedFile);
+                                    });
+                                }
+                            } else {
+                                throw new Error(req.statusText);
+                            }
+                        }; // onload
+
+                        req.onerror = function(e){
+                            throw new Error(req.statusText);
+                        };
+                        
+                        req.send();
+
+                    } else {
+                        throw new Error("error prepping file: " + file);
+                    }
+                }); // promise
+            } // http req
+
+            // add httpReq promisses to reqPromises
+            audioSources.forEach(function(source){
+                    reqPromises.push(httpReq(source, audioSources));
+                });
+            
+            // check that all httpReq promises have been completed, post process
+            return Promise.all(reqPromises).then(function(dataArrays){
+               var dataObject = {};
+                dataArrays.forEach(function(e){
+                    dataObject[e[0]] = e[1];
+                });
+                resolve(dataObject);
+            }).catch(function(err){
+                reject(err);
+            });
+
+        }); // decode promise
+    } // decode audio sources
+    
+    
+    // playback method
+    this.playback = function(buffer){
+        var source = ctx.createBufferSource();
+        source.buffer = buffer;
+        source.connect(ctx.destination);
+        source.start(0);
+    }; // playback
+    
+} //decibel
